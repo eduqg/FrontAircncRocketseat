17fe58b5e8bc725384d48dd94ba09b5ae33885ef
eduqg <eduardoqgomes@gmail.com>
2019-10-02 19:02:47 -0300

    Html and css to new Spot

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import camera from '../../assets/camera.svg';
+import './styles.css';
 
 export default function New() {
+  const [company, setCompany] = useState('');
+  const [techs, setTechs] = useState('');
+  const [price, setPrice] = useState('');
+
+  function handleSubmit() {
+
+  }
+
   return (
-    <div />
+    <form onSubmit={handleSubmit}>
+      <label id="thumbnail">
+        <input type="file" />
+        <img src={camera} alt="Select img" />
+      </label>
+
+      <label htmlFor="company">EMPRESA *</label>
+      <input
+        id="company"
+        placeholder="Sua empresa incrível"
+        value={company}
+        onChange={(event) => setCompany(event.target.value)}
+      />
+
+      <label htmlFor="company">TECNOLOGIAS * <span>(separadas por vírgula)</span></label>
+      <input
+        id="techs"
+        placeholder="Quais tecnologias são usadas"
+        value={techs}
+        onChange={(event) => setTechs(event.target.value)}
+      />
+
+      <label htmlFor="company">DIÁRIA * <span>(em branco para gratuito)</span></label>
+      <input
+        id="price"
+        placeholder="Valor cobrado por dia"
+        value={price}
+        onChange={(event) => setPrice(event.target.value)}
+      />
+
+      <button type="submit" className="btn">Cadastrar</button>
+
+    </form>
   );
 }
