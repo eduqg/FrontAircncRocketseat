678d4a6c2b97ff5128b6a93bed62cca78407b914
eduqg <eduardoqgomes@gmail.com>
2019-10-02 17:05:57 -0300

    Html and css for Login

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import './App.css';
 import logo from './assets/logo.svg';
 function App() {
   return (
-    <div className="conteiner">
-      <img src={logo} alt="AircncLogo" />
+    <div className='container'>
+      <img src={logo} alt='AircncLogo' />
+      <div className='content'>
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa.
+        </p>
+        <form action="">
+          <label htmlFor="email">E-mail *</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Seu melhor e-mail"
+          />
+          <button type="submit" className="btn">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
